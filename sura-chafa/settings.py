5efb0bc69d698a2cac33377ade3a5d5283608676
Vega3 <danimt3705@gmail.com>
2024-07-24 18:15:16 -0500

    Se agrego las settings de la BD

---
+++ sura-chafa/settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sura-chafa',
+        'USER': 'root',
+        'PASSWORD': 'Morales10',
+        'HOST': 'localhost',  # O la dirección IP de tu servidor MySQL
+        'PORT': '3306',       # Puerto por defecto para MySQL
+    }
+}
