bbfbdb61374ab4c556521ab172298b732329e2e3
Vega3 <danimt3705@gmail.com>
2024-07-24 23:45:03 -0500

    was added ReservaCitaForm

--- users/forms.py
+++ users/forms.py
@@ -1,8 +1,15 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import Usuario
+from .models import Cita
 
 class RegistroForm(UserCreationForm):
     class Meta:
         model = Usuario
         fields = ['username', 'password1', 'password2', 'role']
+
+
+class ReservaCitaForm(forms.ModelForm):
+    class Meta:
+        model = Cita
+        fields = ['fecha', 'medico']
