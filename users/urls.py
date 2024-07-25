7d285367479df99219d1cff108b9696685ad7c6a
Vega3 <danimt3705@gmail.com>
2024-07-24 23:58:55 -0500

    An entry was added for the new reserva_cita view

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from . import views
 from .views import login, patient_home
 
 urlpatterns = [
     path('login/', login, name='login'),
     path('patient_home/', patient_home, name='patient_home'),
+    path('reserva_cita/', views.reserva_cita, name='reserva_cita'),
 ]
