36f546a0db66aa5076391224147333d2ced44e54
Vega3 <danimt3705@gmail.com>
2024-07-24 23:50:32 -0500

    added a view to handle appointment booking

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import login as auth_login, authenticate
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm  
+from .forms import ReservaCitaForm
 
 def login(request):
     if request.method == 'POST':
@@ -15,3 +16,14 @@ def login(request):
 
 def patient_home(request):
     return render(request, 'patient_home.html')
+
+
+def reserva_cita(request):
+    if request.method == 'POST':
+        form = ReservaCitaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('patient_home')  # Redirige a la vista patient_home después de la reserva
+    else:
+        form = ReservaCitaForm()
+    return render(request, 'reserva_cita.html', {'form': form})
