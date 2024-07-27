646cf3ec58a641536c0352e302b21dfe648ce09b
Mariana Vélez Cuéllar <velez.7w7.com>
2024-07-27 12:44:14 -0500

    Creación página Home - pacientes y mejoras diseño registro

--- sura-chafa/settings.py
+++ sura-chafa/settings.py
@@ -4,8 +4,8 @@ DATABASES = {
         'NAME': 'sura-chafa',
         'USER': 'root',
         'PASSWORD': 'Morales10!@#',
-        'HOST': 'localhost',  # O la dirección IP de tu servidor MySQL
-        'PORT': '3306',       # Puerto por defecto para MySQL
+        'HOST': 'localhost',  
+        'PORT': '3306',      
     }
 }
 
